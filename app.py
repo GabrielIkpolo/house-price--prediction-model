cd4b91d0ecf101dbc1528c2b6bc229934bd4f801
GabrielIkpolo <contact.gikps@gmail.com>
2024-10-06 13:59:25 -0400

    Added scalar used for scalling

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 
 # Load the trained model
 model = joblib.load('./model/trained_model.pkl')
+scaler = joblib.load('./model/scaler.pkl')
 
+numerical_features = ['bedrooms', 'bathrooms','sqft_living','sqft_lot', 'floors', 'waterfront', 'view', 'condition', 'grade','sqft_above','sqft_basement', 'yr_built', 'yr_renovated', 'zipcode', 'lat', 'long','sqft_living15','sqft_lot15']
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -14,6 +16,11 @@ def predict():
     data= request.get_json()
     # Create a pandas dataframe from the imputs
     input_df = pd.DataFrame(data)
+
+     # Scale the input features using the same StandardScaler object
+    input_df[numerical_features] = scaler.transform(input_df[numerical_features])
+
+   
     # make prediction using the model
     prediction = model.predict(input_df)
     
